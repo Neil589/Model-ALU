f22140cc81dafdad5598ce6e87e3ed34e90a45da
Neil Thupili <neil557@live.unc.edu>
2021-11-12 11:28:52 -0500

    a6

--- alu.c
+++ alu.c
@@ -12,9 +12,8 @@
 // Simulate add and subtract arithmetic digital logic circuit
 // 
 // ----------------------------------------
-
 void arithmetic_fun_exe(arithmetic* circuit){
-    // TODO: put your code here!
+    circuit->Y = circuit->Sub == '0' ? circuit->A + circuit->B : circuit->A - circuit->B;
 }
 
 int arithmetic_fun_cfg(arithmetic* circuit){
@@ -29,9 +28,8 @@ int arithmetic_fun_cfg(arithmetic* circuit){
 // Simulate shifting digital logic circuit
 // 
 // ----------------------------------------
-
 void shift_fun_exe(shift* circuit){
-    // TODO: put your code here!
+    circuit->Y = strcmp(circuit->Bool, "00") == 0 ? circuit->B << circuit->A : circuit->B >> circuit->A;
 }
 
 int shift_fun_cfg(shift* circuit){
@@ -40,16 +38,26 @@ int shift_fun_cfg(shift* circuit){
     }else{
         return OK;
     }
-
 }
 
 // ----------------------------------------
 // Simulate boolean logic digital logic circuit
 // 
 // ----------------------------------------
-
 void logic_fun_exe(logic* circuit){
-    // TODO: put your code here!
+    if(circuit->Bool[0] == '0'){
+        if(circuit->Bool[1] == '0'){
+            circuit->Y = circuit->A & circuit->B;
+        }else{
+            circuit->Y = circuit->A | circuit->B;
+        }
+    }else{
+        if(circuit->Bool[1] == '0'){
+            circuit->Y = circuit->A ^ circuit->B;
+        }else{
+            circuit->Y = ~(circuit->A | circuit->B);
+        }
+    }
 }
 
 int logic_fun_cfg(logic* circuit){
@@ -64,9 +72,8 @@ int logic_fun_cfg(logic* circuit){
 // Simulate multiplexor (MUX) digital logic circuit
 // 
 // ----------------------------------------
-
 void mux_fun_exe(mux* circuit){
-    // TODO: put your code here!
+    circuit->Y = circuit->Selector == '0' ? circuit->A : circuit->B;
 }
 
 int mux_fun_cfg(mux* circuit){
@@ -87,7 +94,6 @@ int mux_fun_cfg(mux* circuit){
 // return:
 //			None
 // ----------------------------------------
-
 void initialize_alu() {
     math_mux = (mux*)malloc(sizeof(mux));
     shft_mux = (mux*)malloc(sizeof(mux));
@@ -138,12 +144,18 @@ void initialize_alu() {
 //			CONFIG_ERROR for an unsupported configuration value
 //        	PASS when all configuration values are correct
 // ----------------------------------------
-
 int configure_alu(char Sub, char* Bool, char Shft, char Math){
-    // TODO: put your code here!
-
-    return 0;
-
+    arithmetic_circuit->Sub = Sub;
+    strcpy(shift_circuit->Bool, Bool);
+    strcpy(logic_circuit->Bool, Bool);
+    shft_mux->Selector = Shft;
+    math_mux->Selector = Math;
+    return (arithmetic_fun_cfg(arithmetic_circuit) == OK 
+        && shift_fun_cfg(shift_circuit) == OK
+        && logic_fun_cfg(logic_circuit) == OK
+        && mux_fun_cfg(shft_mux) == OK
+        && mux_fun_cfg(math_mux) == OK) 
+        ? OK : CONFIG_ERROR;
 } // end configure_alu() function
 
 // ----------------------------------------
@@ -156,9 +168,25 @@ int configure_alu(char Sub, char* Bool, char Shft, char Math){
 // return:
 //			Result = signed 2's complement 32 bit integer value
 // ----------------------------------------
-
 int run_alu(int A, int B){
-    // TODO: put your code here!
-    return 0;
+    // Set
+    arithmetic_circuit->A = shift_circuit->A = logic_circuit->A = A;
+    arithmetic_circuit->B = shift_circuit->B = logic_circuit->B = B;
+    // Clock
+    arithmetic_fun_exe(arithmetic_circuit);
+    shift_fun_exe(shift_circuit);
+    logic_fun_exe(logic_circuit);
+    // Set
+    shft_mux->A = logic_circuit->Y;
+    shft_mux->B = shift_circuit->Y;
+    // Clock
+    mux_fun_exe(shft_mux);
+    // Set
+    math_mux->A = shft_mux->Y;
+    math_mux->B = arithmetic_circuit->Y;
+    // Clock
+    mux_fun_exe(math_mux);
+    return math_mux->Y;
 } // end run_alu() function
+
 
